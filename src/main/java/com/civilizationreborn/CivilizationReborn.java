ad260aa7e42333127d1337e32661ef2a8e4848aa
mat37dev <mathieu.crosnier15@outlook.fr>
2024-11-11 11:45:28 +0100

    Correction de l'erreur lors du build de l'item

--- src/main/java/com/civilizationreborn/CivilizationReborn.java
+++ src/main/java/com/civilizationreborn/CivilizationReborn.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CivilizationReborn implements ModInitializer {
-	public static final String MOD_ID = "civilization_reborn";
+	public static final String MOD_ID = "civilization-reborn";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
--- src/main/java/com/civilizationreborn/item/CommonItems.java
+++ src/main/java/com/civilizationreborn/item/CommonItems.java
@@ -5,18 +5,24 @@ import com.civilizationreborn.CivilizationReborn;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
 
 
 public class CommonItems {
     //Création et enregistrement d'un item
-    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item PINK_GARNET = registerItem("pink_garnet",Item::new, new Item.Settings());
 
-
-    public static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(CivilizationReborn.MOD_ID, name), item);
+    public static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CivilizationReborn.MOD_ID, name));
+        return Items.register(registryKey, factory, settings);
     }
 
     public static void registerCommonItems(){
